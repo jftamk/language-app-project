dfe34618df6c7c0af27cfa179be99d0fbaf98b74
Joni Forss <joni.forss@tuni.fi>
2022-01-08 16:39:04 +0200

    Edit: Add pool connection/release

--- database/crudrepository.js
+++ database/crudrepository.js
@@ -2,6 +2,7 @@ const mysql = require("mysql");
 
 require("dotenv").config();
 
+//Info in env file
 const pool = mysql.createPool({
   connectionLimit: 10,
   host: process.env.DB_HOST,
@@ -9,3 +10,16 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DB,
 });
+
+//Create connection using pool
+pool.on("acquire", function (connection) {
+  console.log("Connection %d acquired", connection.threadId);
+});
+//Release connection using pool
+pool.on("release", function (connection) {
+  console.log("Connection %d released", connection.threadId);
+});
+
+let connection = {};
+
+module.exports = connection;
