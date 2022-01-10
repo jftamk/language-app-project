ff18b0c60dbd3a5c23e32a032b60e7f061a3f097
Joni Forss <joni.forss@tuni.fi>
2022-01-10 23:22:45 +0200

    Add: AdminTable Delete button

--- frontend/src/AdminTable.js
+++ frontend/src/AdminTable.js
@@ -1,14 +1,27 @@
 import React from "react";
-
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
 import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
 function AdminTable(props) {
   //Take props from admin.js and return as tablebody while mapping the database.
+  //props.deleteWord --> call admin.js to trigger delete function with current id and the matching word.
   return (
     <TableRow>
       <TableCell>{props.eng}</TableCell>
       <TableCell>{props.fin}</TableCell>
       <TableCell>{props.tag}</TableCell>
+      <TableCell>
+        <IconButton
+          aria-label="delete"
+          size="small"
+          onClick={() => {
+            props.deleteWord(props.id);
+          }}
+        >
+          <DeleteIcon />
+        </IconButton>
+      </TableCell>
     </TableRow>
   );
 }
