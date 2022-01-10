deb956b1575034b6b2982c41128c6344c4acb16d
Joni Forss <joni.forss@tuni.fi>
2022-01-10 23:50:51 +0200

    Add: onchange Event

--- frontend/src/table.js
+++ frontend/src/table.js
@@ -5,13 +5,24 @@ import TableRow from "@mui/material/TableRow";
 import Input from "@mui/material/Input";
 function TableComp(props) {
   const { type, name, placeholder } = props;
-
+  async function afterInput(e) {
+    const a = e.target.value;
+    //If input === finnish word from database, call onChanges function in user.js --> TODO onChanges, user.js
+    if (a === props.finnish) {
+      props.onChanges(a);
+    }
+  }
   return (
     <>
       <TableRow>
         <TableCell>{props.eng}</TableCell>
         <TableCell>
-          <Input placeholder={placeholder} type="text" name={name} />
+          <Input
+            placeholder={placeholder}
+            type="text"
+            name={name}
+            onBlur={afterInput}
+          />
         </TableCell>
       </TableRow>{" "}
     </>
