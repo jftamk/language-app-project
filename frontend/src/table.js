4bc2ad1b175db2c328af52a66b6163327f2206ce
Joni Forss <joni.forss@tuni.fi>
2022-01-11 23:50:11 +0200

    Fix: Remove items not in use

--- frontend/src/table.js
+++ frontend/src/table.js
@@ -1,15 +1,19 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import USER from "./user";
 import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
 import Input from "@mui/material/Input";
 function TableComp(props) {
   const { type, name, placeholder } = props;
+
   async function afterInput(e) {
     const a = e.target.value;
-    //If input === finnish word from database, call onChanges function in user.js --> TODO onChanges, user.js
+
+    //If input === finnish word from database, call onChanges function in user.js
     if (a === props.finnish) {
-      props.onChanges(a);
+      props.onChanges(a, props.eng);
+    } else {
+      props.onWrong(a, props.eng);
     }
   }
   return (
