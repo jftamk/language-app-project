57902967c16871aaa50e1bad436bed7a8c5e18c5
Joni Forss <joni.forss@tuni.fi>
2022-01-13 00:14:26 +0200

    Add: word editing

--- frontend/src/AdminTable.js
+++ frontend/src/AdminTable.js
@@ -3,6 +3,13 @@ import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
 import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
+import { FormControl } from "@mui/material";
+import { InputLabel } from "@mui/material";
+import { Select } from "@mui/material";
+import { MenuItem } from "@mui/material";
+import Input from "@mui/material/Input";
+import Button from "@mui/material/Button";
+import EditIcon from "@mui/icons-material/Edit";
 function AdminTable(props) {
   //Take props from admin.js and return as tablebody while mapping the database.
   //props.deleteWord --> call admin.js to trigger delete function with current id and the matching word.
@@ -20,7 +27,57 @@ function AdminTable(props) {
           }}
         >
           <DeleteIcon />
-        </IconButton>
+        </IconButton>{" "}
+        {props.id === props.editing ? (
+          <>
+            {" "}
+            <Input
+              type="text"
+              placeholder="English word"
+              value={props.EditEng}
+              onChange={(e) => props.setEditEng(e.target.value)}
+            />{" "}
+            <Input
+              type="text"
+              placeholder="Finnish word"
+              onChange={(e) => props.setEditFin(e.target.value)}
+            />{" "}
+            <FormControl sx={{ m: 3, width: 150 }}>
+              <InputLabel id="simple-select-label">Category</InputLabel>
+              <Select
+                labelId="simple-select-label"
+                id="simple-select"
+                label="category"
+                onChange={(e) => props.setEditTAG(e.target.value)}
+              >
+                <MenuItem value={"Colors"}>Colors</MenuItem>
+                <MenuItem value={"Animals"}>Animals</MenuItem>
+                <MenuItem value={"Vehicles"}>Vehicles</MenuItem>
+                <MenuItem value={"Other"}>Other</MenuItem>
+              </Select>
+            </FormControl>
+          </>
+        ) : (
+          <br></br>
+        )}
+        {props.id === props.editing ? (
+          <>
+            <Button onClick={() => props.editTodo(props.id)}>
+              Finish changes
+            </Button>
+            <Button
+              variant="contained"
+              color="warning"
+              onClick={() => props.setediting(null)}
+            >
+              Cancel
+            </Button>
+          </>
+        ) : (
+          <Button onClick={() => props.setediting(props.id)}>
+            <EditIcon />
+          </Button>
+        )}
       </TableCell>
     </TableRow>
   );
