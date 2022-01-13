24452fea76a9eb860903fc2825031a57e7ccfb4d
Joni Forss <joni.forss@tuni.fi>
2022-01-13 20:24:02 +0200

    edit for heroku

--- database/crudrepository.js
+++ database/crudrepository.js
@@ -5,10 +5,10 @@ require("dotenv").config();
 //Info in env file
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DB,
+  host: "mydb.tamk.fi",
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
 });
 
 //Create connection using pool
