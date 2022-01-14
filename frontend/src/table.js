7159a1768f2bde284142e66f0356fee471c1ecb5
Joni Forss <joni.forss@tuni.fi>
2022-01-14 18:22:31 +0200

    Edit comments

--- frontend/src/table.js
+++ frontend/src/table.js
@@ -1,26 +1,33 @@
-import React, { useState, useEffect } from "react";
-import USER from "./user";
+import React from "react";
 import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
 import Input from "@mui/material/Input";
 function TableComp(props) {
-  const { type, name, placeholder } = props;
+  const { name, placeholder } = props;
 
   async function afterInput(e) {
-    const ab = e.target.value;
-    const abc = ab.toLowerCase();
-    const removeExtraSpace = (s) => s.trim().split(/ +/).join(" ");
+    const ab = e.target.value; //Take input as variable
+    const abc = ab.toLowerCase(); //Change letters to lowercase
 
+    //Remove spaces after the written word if there's any.
+    const removeExtraSpace = (s) => s.trim().split(/ +/).join(" ");
     const a = removeExtraSpace(abc);
+
     //If input === finnish word from database, call onChanges function in user.js
     if (a === props.finnish) {
       props.onChanges(a, props.eng);
     } else {
+      //If wrong answer, give the answer to onWrong function in user.js
       props.onWrong(a, props.eng);
     }
   }
   return (
     <>
+      {" "}
+      {/**Table row for each word,
+       * after user has written their answer, call afterInput function.
+       * This table is for displaying english words and taking finnish words as input.
+       */}
       <TableRow>
         <TableCell>{props.eng}</TableCell>
         <TableCell>
