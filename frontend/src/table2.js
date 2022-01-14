2ff40aae03cd3352397ba737ad2ce803201a7e39
Joni Forss <joni.forss@tuni.fi>
2022-01-14 18:22:53 +0200

    Edit comments

--- frontend/src/table2.js
+++ frontend/src/table2.js
@@ -1,26 +1,34 @@
-import React, { useState } from "react";
+import React from "react";
 import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
 import Input from "@mui/material/Input";
 
 function TableComp2(props) {
-  const { type, name, placeholder } = props;
+  const { name, placeholder } = props;
   async function afterInput(e) {
-    const ab = e.target.value;
-    const abc = ab.toLowerCase();
-    const removeExtraSpace = (s) => s.trim().split(/ +/).join(" ");
+    const ab = e.target.value; //Take input as variable
+    const abc = ab.toLowerCase(); //Change letters to lowercase
 
+    //Remove spaces after the written word if there's any.
+    const removeExtraSpace = (s) => s.trim().split(/ +/).join(" ");
     const a = removeExtraSpace(abc);
+
     //If input === finnish word from database, call onChanges function in user.js
     if (a === props.eng) {
       props.onChanges(a, props.finnish);
     } else {
+      //If wrong answer, give the answer to onWrong function in user.js
       props.onWrong(a, props.finnish);
     }
   }
 
   return (
     <>
+      {" "}
+      {/**Table row for each word,
+       * after user has written their answer, call afterInput function.
+       * This table is to display finnish words and taking english words as input.
+       */}
       <TableRow>
         <TableCell>
           <Input
